c4139b2268392a25852f83dde482cadd8d307492
wiktoriamroczko <s215605@student.uek.krakow.pl>
2020-03-25 14:48:13 +0100

    WM 2020-03-25

--- scraper.py
+++ scraper.py
@@ -1,55 +1,86 @@
 #import bibliotek
+import pprint
 import requests
 from bs4 import BeautifulSoup
+import json
 
 #adres URL przykładowej strony z opiniami
-#URL = "https://www.ceneo.pl/59016320#tab=reviews"
-URL = "https://www.ceneo.pl/76891701#tab=reviews"
+url_prefix = "https://www.ceneo.pl"
+product_id = input("Podaj kod produktu: ")
+url_postfix = "#tab=reviews"
+URL = url_prefix + "/"+ product_id + url_postfix
 
-#pobranie kou html strony z podanego URL
-page_respons = requests.get(URL)
-page_tree = BeautifulSoup(page_respons.text, 'html.parser')
+#pusta lista na wszystkie opinie o produkcie
+opinions_list = []
 
-#wydobycie z koodu html strony fragmentów odpowiadającycm poszczególnym opiniom
-opinions = page_tree.find_all("li", "review-box")
+while URL:
 
-#wydobycie składowych dla pojedynczej opinii
-#opinion = opinions.pop()
+    #pobranie kou html strony z podanego URL
+    page_respons = requests.get(URL)
+    page_tree = BeautifulSoup(page_respons.text, 'html.parser')
 
-for opinion in opinions:
-    opinion_id = opinion["data-entry-id"]
-    author = opinion.find("div", "reviewer-name-line").string
-    recommendation = opinion.find("div", "product-review-summary").find("em").string
-    stars = opinion.find("span", "review-score-count").string
-    try:
-        purchased = opinion.find("div", "product-review-pz").string
-    except AttributeError:
-        purchased = None
-    dates = opinion.find("span","review-time").find_all("time")
-    review_date = dates.pop(0)["datetime"]
-    try:
-        purchase_date= dates.pop(0)["datatime"]
-    except IndexError:
-        purchased = None
-    useful = opinion.find("button", "vote-yes").find("span").string
-    useless = opinion.find("button", "vote-no").find("span").string
-    content = opinion.find("p", "product-review-body").get_text()
-    try:
-        pros = opinion.find("div","pros-cell").find('ul').get_text()
-    except AttributeError:
-        pros= None
-    try:
-        cons = opinion.find("div","cons-cell").find('ul').get_text()
-    except AttributeError:
-        cons = None
+    #wydobycie z koodu html strony fragmentów odpowiadającycm poszczególnym opiniom
+    opinions = page_tree.find_all("li", "review-box")
+
     
-print(cons)
+    #wydobycie składowych dla pojedynczej opinii
 
+    for opinion in opinions:
+        opinion_id = opinion["data-entry-id"]
+        author = opinion.find("div", "reviewer-name-line").string
+        try:
+            recommendation = opinion.find("div","product-review-summary").find("em").string
+        except AttributeError:
+            recommendation = None
+        stars = opinion.find("span", "review-score-count").string
+        try:
+            purchased = opinion.find("div", "product-review-pz").find("em").string
+        except AttributeError:
+            purchased = None
+        dates = opinion.find("span", "review-time").find_all("time")
+        review_date = dates.pop(0)["datetime"]
+        try:
+            purchase_date = dates.pop(0)["datetime"]
+        except IndexError:
+            purchase_date = None
+        useful = opinion.find("button","vote-yes").find("span").string
+        useless = opinion.find("button","vote-no").find("span").string
+        content = opinion.find("p","product-review-body").get_text()
+        try:
+            pros = opinion.find("div", "pros-cell").find("ul").get_text()
+        except AttributeError:
+            pros = None
+        try:
+            cons = opinion.find("div", "cons-cell").find("ul").get_text()
+        except AttributeError:
+            cons = None
+    
+    opinion_dict = {
+        "opinion_id":opinion_id,
+        "recommendation":recommendation,
+        "stars":stars,
+        "content":content,
+        "author":author,
+        "pros":pros,
+        "cons":cons,
+        "useless":useless,
+        "useful":useful,
+        "purchased":purchased,
+        "purchase_date":purchase_date,
+        "review_date":review_date
+    }
 
-# - identyfikator: li.review-box["data-entry-id"]
-# - data wystawienia: span.review-time > time["datetime"] - pierwszy element listy
-# - data zakupu: span.review-time > time["datetime"] - drugi element listy
-# - treść: p.product-review-body
-# - wady: div.cons-cell > ul
-# - zalety: div.pros-cell > ul
-# zrobbic date wady i zalety i zeby wszystkie opinie sie pobieraly i byly zapisywane
+    opinions_list.append(opinion_dict)
+
+    try:
+        URL = url_prefix + page_tree.find("a", "pagination__next")["href"]
+    except TypeError:
+        URL = None 
+    print(URL)
+
+with open(product_id+".json", "w", encoding = "utf-8") as fp:
+    json.dump(opinions_list, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
+
+print(len(opinions_list))
+
+#pprint.pprint(opinions_list)
